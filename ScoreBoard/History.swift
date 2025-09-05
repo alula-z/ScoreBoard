3874885228440859c60418287b25588282a2ba3d
alula-z <azeruesenay14@gmail.com>
2025-09-04 21:30:40 -0400

    green for winning team and red for losing team

--- ScoreBoard/History.swift
+++ ScoreBoard/History.swift
@@ -14,23 +14,59 @@ struct History: View{
             animation: .default
         ) private var games: FetchedResults<GameEntity>
     var body: some View{
-            List{
-                if games.isEmpty{
-                    Text("No games saved")
-                        .foregroundStyle(Color.gray)
-                }else{
-                    ForEach(games, id: \.self){game in
-                        HStack(alignment: .center){
-                            Text("\(game.teamA ?? "")")
-                            Spacer()
-                            Text("\(game.scoreA) - \(game.scoreB)")
-                            Spacer()
-                            Text("\(game.teamB ?? "")")
+        ZStack{
+            VStack{
+                Text("History")
+                    .font(.system(size: 30))
+                    .fontWeight(.bold)
+                List{
+                    if games.isEmpty{
+                        Text("No games saved")
+                            .foregroundStyle(Color.gray)
+                    }else{
+                        ForEach(games, id: \.self){game in
+                            HStack(alignment: .center){
+                                Text("\(game.teamA ?? "")")
+                                Spacer()
+                                game.scoreA > game.scoreB ?
+                                HStack{
+                                    Text("\(game.scoreA)")
+                                        .foregroundStyle(Color.green)
+                                        .fontWeight(.bold)
+                                    Text(" - ")
+                                    Text("\(game.scoreB)")
+                                        .foregroundStyle(Color.red)
+                                }
+                                
+                                : game.scoreB > game.scoreA ?
+                                HStack{
+                                        Text("\(game.scoreA)")
+                                            .foregroundStyle(Color.red)
+                                        Text(" - ")
+                                        Text("\(game.scoreB)")
+                                            .foregroundStyle(Color.green)
+                                            .fontWeight(.bold)
+                                    }
+                                :
+                                HStack{
+                                        Text("\(game.scoreA)")
+                                            .foregroundStyle(Color.gray)
+                                        Text(" - ")
+                                        Text("\(game.scoreB)")
+                                            .foregroundStyle(Color.gray)
+                                    }
+                                Spacer()
+                                Text("\(game.teamB ?? "")")
+                            }
+                            .frame(width: .infinity)
                         }
-                        .frame(width: .infinity)
+                        
                     }
-                    
+                        
                 }
+                
+            }
+            
         }
     }
 }
